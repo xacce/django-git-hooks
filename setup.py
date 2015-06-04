d42c62ab2114de85dd90986424f12b743174c8fb
dezu <zhukovvitaliy@mail.ru>
2015-06-04 19:54:06 +0600

    Start

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-git-hooks',
-    version='0.2.0',
+    version='0.2.1',
     packages=['git_hooks', ],
     requires=['django (>= 1.3)', ],
     url='https://github.com/snznaota/django-git-hooks.git',
